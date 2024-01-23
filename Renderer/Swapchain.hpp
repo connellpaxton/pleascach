0bd5a1780a479e83255a7b33e43cdafe97c083f8
Conál <hopless@tyromancy.art>
2024-01-23 16:03:00 -0500

    Fixed linux build issue caused by ommision of <memory> header

--- Renderer/Swapchain.hpp
+++ Renderer/Swapchain.hpp
@@ -3,6 +3,7 @@
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 #include <vulkan/vulkan.hpp>
 
+#include <memory>
 
 #include <Renderer/RenderPass.hpp>
 
--- Resources/Texture.hpp
+++ Resources/Texture.hpp
@@ -4,6 +4,7 @@
 #include <vulkan/vulkan.hpp>
 
 #include <Memory/Image.hpp>
+#include <memory>
 
 struct CommandBuffer;
 
